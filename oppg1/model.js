72a7ea3d88d5da46dffd084ba79960b0f0194497
camillahu <camillahaugenurdal@gmail.com>
2024-03-25 21:50:15 +0100

    Update model.js

--- oppg1/model.js
+++ oppg1/model.js
@@ -6,11 +6,9 @@ const currentDate = new Date().getDate();
 console.log(currentDate)
 
 const easterDate = new Date("Mar 30, 2024").getDate();
-// easterDate = Number(easterDate)
 console.log(easterDate)
 
-let counter = 4
-// counter = Number(counter)
+let counter = easterDate - currentDate;
 let funfact = "";
 
 
